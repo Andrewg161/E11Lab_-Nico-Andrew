7df2db7763aad2c0bdf2d4575cc96f8c6f893064
Andrewg161 <andrewg161@berkeley.edu>
2025-02-07 16:23:17 -0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -7,11 +7,20 @@ bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
 bme680.sea_level_pressure = 1013.25
 
-while True:
+start_time = time.time()
+run_time = 30
+while time.time() - start_time < run_time:
+	elasped_time = time.time() - start_time
 	print("\nTemperature: %0.1f C" % bme680.temperature)
 	print("Gas: %d ohm" % bme680.gas)
 	print("Humidity: %0.1f %%" % bme680.relative_humidity)
 	print("Pressure: %0.3f hPa" % bme680.pressure)
 	print("Altitude = %0.2f meters" % bme680.altitude)
 	
+	
+	print("Start time:", time.ctime(start_time))
 	time.sleep(2)
+	end_time = time.time()
+	print("End time:", time.ctime(end_time))
+	print("Total time:", end_time - start_time, "seconds")
+	
