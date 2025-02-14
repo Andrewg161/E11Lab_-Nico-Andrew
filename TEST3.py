4b37d2b6519dfa855c148ff974fb37cd9fce58e5
Andrewg161 <andrewg161@berkeley.edu>
2025-02-14 14:56:50 -0800

    Update TEST3.py

--- TEST3.py
+++ TEST3.py
@@ -1,3 +1,8 @@
+import time
+import board
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_pm25.i2c import PM25_I2C
 import serial
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 from adafruit_pm25.uart import PM25_UART
