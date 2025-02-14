40369dc31ecfc35c25d967851ea069c1af7fc37a
Andrewg161 <andrewg161@berkeley.edu>
2025-02-14 14:30:34 -0800

    Update test214.py

--- test214.py
+++ test214.py
@@ -5,11 +5,11 @@ file = open("test.csv", "w", newline=None)
 
 file_writer = csv.writer(file)
 
-file_writer.writerrow(["Time", "Data"])
+file_writer.writerow(["Time", "Data"])
 
 for i in range(10):
   entry = np.random.random()
   now = time.time()
-  file_writer.writerrow([now, entry])
+  file_writer.writerow([now, entry])
 
 file.close()
