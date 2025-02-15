1e4eab4e54c386cb99eee13aaea9f23a8b726ec7
Andrewg161 <andrewg161@berkeley.edu>
2025-02-14 16:25:37 -0800

    Update TEST3.py

--- TEST3.py
+++ TEST3.py
@@ -90,10 +90,5 @@ while time.time() - start_time < run_time:
     print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
     print("---------------------------------------")
 
-	print("Start time:", time.ctime(start_time))
-		end_time = time.time()
-	print("End time:", time.ctime(end_time))
-	print("Total time:", end_time - start_time, "seconds")
-
 file.close()
 
