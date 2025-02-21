086b60aa267ea4c31bdda335938fe3412401533a
Andrewg161 <andrewg161@berkeley.edu>
2025-02-21 14:35:50 -0800

    Update test_inputs.py

--- test_inputs.py
+++ test_inputs.py
@@ -3,13 +3,14 @@ import time
 
 print(sys.argv)
 
+run_time = 10 
 if len(sys.argv) < 2:
   print('Script requieres run_time(int) as an input')
-  exit()
+  #exit()
 else:
   run_time = int(sys.argv[1])
 
-run_time = int(sys.argv[1])
+#run_time = int(sys.argv[1])
 
 count = 0 
 while coutn < run_time:
