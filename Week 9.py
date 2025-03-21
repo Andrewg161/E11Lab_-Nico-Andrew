214687b2443774da529d3f1b5c58fa8e62de3cf5
Andrewg161 <andrewg161@berkeley.edu>
2025-03-21 15:50:44 -0700

    Update Week 9.py

--- Week 9.py
+++ Week 9.py
@@ -10,13 +10,11 @@ def pulse_detected(channel):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print("Pulse detected at", timestamp)
 
-#The Set up for GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SIGNAL_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(SIGNAL_PIN, GPIO.FALLING, callback=pulse_detected)
 print("Running")
 
-#WHile Loop
 while True:
     start_time = datetime.datetime.now()
     while (datetime.datetime.now() - start_time).seconds < 60:
