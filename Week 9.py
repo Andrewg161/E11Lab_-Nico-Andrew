fd4f96e82c0f0433e0fd77c41a9f1ddcc3aaa478
Andrewg161 <andrewg161@berkeley.edu>
2025-03-21 15:52:39 -0700

    Update Week 9.py

--- Week 9.py
+++ Week 9.py
@@ -15,11 +15,41 @@ GPIO.setup(SIGNAL_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(SIGNAL_PIN, GPIO.FALLING, callback=pulse_detected)
 print("Running")
 
+import RPi.GPIO as GPIO
+import datetime
+
+SIGNAL_PIN = 2
+count = 0
+
+def pulse_detected(channel):
+    global count
+    count += 1
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print("Pulse detected at", timestamp)
+
+#The Set up for GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SIGNAL_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(SIGNAL_PIN, GPIO.FALLING, callback=pulse_detected)
+print("Running")
+
+#WHile Loop
+start_time = datetime.datetime.now()
+count = 0
+
+# While loop 
 while True:
-    start_time = datetime.datetime.now()
-    while (datetime.datetime.now() - start_time).seconds < 60:
-        None
-    print("Counts in the last minute:", count)
-    count = 0 
+    current_time = datetime.datetime.now()
+    elapsed_time = (current_time - start_time).seconds
+
+    if elapsed_time < 60:
+        if GPIO.input(SIGNAL_PIN) == GPIO.LOW:
+            count += 1
+    else:
+        print("Counts in the last minute:", count)
+        count = 0 
+        start_time = datetime.datetime.now() 
+        
 GPIO.cleanup()
+
 
