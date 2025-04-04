9cce07d6d830087ca6e953825984fbfa354afa1f
Andrewg161 <andrewg161@berkeley.edu>
2025-04-04 15:13:04 -0700

    Update Week9.py

--- Week 9/Week9.py
+++ Week 9/Week9.py
@@ -1,9 +1,16 @@
 import RPi.GPIO as GPIO
 import datetime
 import time
+import csv 
 
 SIGNAL_PIN = 6
 count = 0
+
+# Making the CSV_Writerow
+output_file = "counts_output.csv"
+csv_file = open(output_file, mode='w', newline='')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(["Timestamp", "Counts"])
 
 def pulse_detected(channel):
     global count
@@ -23,5 +30,9 @@ while True:
     while (datetime.datetime.now() - start_time).seconds < 120:
         None  
     print("Counts in the last minute:", count)
+    
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    csv_writer.writerow([timestamp, count]) #Adding the CSV Writer
+    
     count = 0  
-
+csv_file.close() #Close the file
